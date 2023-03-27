a970e4881bd5201706bdf525f3fbf17e0b18660c
U-keun Song <sukeun319@gmail.com>
2023-03-27 23:32:32 +0900

    add login.py and add import to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
+from login import bp
 app = Flask(__name__)
+app.register_blueprint(bp)
 
 from pymongo import MongoClient
 import certifi
