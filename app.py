2369e16a2e4f13d8aa04dfe6862ce6eb3c024e57
U-keun Song <sukeun319@gmail.com>
2023-03-27 21:52:05 +0900

    port 5001

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def movie_get():
     return jsonify({'result':all_movies})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
