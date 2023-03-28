13fa60bfd9ed6ee0105bab0836421c5d4d603a70
U-keun Song <sukeun319@gmail.com>
2023-03-28 11:30:29 +0900

    minor revision

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ client = MongoClient('mongodb+srv://sparta:test@cluster0.xxnpzo1.mongodb.net/?re
 db = client.dbsparta
 
 import requests
-from bs4 import BeautifulSoup
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -108,37 +107,6 @@ def signin():
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-@app.route("/movie", methods=["POST"])
-def movie_post():
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
-    star_receive = request.form['star_give']
-    
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive,headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    ogtitle = soup.select_one('meta[property="og:title"]')['content']
-    ogimage = soup.select_one('meta[property="og:image"]')['content']
-    ogdesc = soup.select_one('meta[property="og:description"]')['content']
-    
-    doc = {
-        'title':ogtitle,
-        'desc':ogdesc,
-        'image':ogimage,
-        'comment':comment_receive,
-        'star':star_receive
-    }
-    db.movies.insert_one(doc)
-    
-    return jsonify({'msg':'저장 완료!'})
-
-@app.route("/movie", methods=["GET"])
-def movie_get():
-    all_movies = list(db.movies.find({},{'_id':False}))
-    return jsonify({'result':all_movies})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
